773d2b44d70538e2a38f98e35b29279d27ffbf97
Oscar Salgado <osalgado@ikerlan.es>
2025-01-12 12:03:03 +0100

    Refactor date and duration models for improved readability and efficiency

--- garminworkouts/models/date.py
+++ garminworkouts/models/date.py
@@ -3,9 +3,10 @@ from datetime import date, timedelta
 
 def get_date(name, race, date_ini) -> tuple[date, int, int]:
     try:
-        week: int = int(name[1:name.index('_')]) if name.startswith('R') else int(name.split('_')[0])
-        week: int = -week if name.startswith('R') or 'D' in name else week
-        day: int = int(name.split('_')[1][0]) if '_' in name else int(name.split('D')[1].split('-')[0])
+        parts = name.split('_')
+        week = int(parts[0][1:] if name.startswith('R') else parts[0])
+        week = -week if name.startswith('R') or 'D' in name else week
+        day = int(parts[1][0]) if len(parts) > 1 else int(name.split('D')[1].split('-')[0])
         return race - timedelta(weeks=week + 1) + timedelta(days=day), week, day
     except ValueError:
         if isinstance(date_ini, dict):
--- garminworkouts/models/duration.py
+++ garminworkouts/models/duration.py
@@ -9,16 +9,14 @@ class Duration:
     duration: str
 
     def get_type(self) -> str:
-        for key, value in TYPE_MAPPING.items():
+        for key in TYPE_MAPPING:
             if key in self.duration:
-                return value
-        if Duration.is_time(self.duration):
-            return 'time'
-        return 'lap.button'
+                return TYPE_MAPPING[key]
+        return 'time' if Duration.is_time(self.duration) else 'lap.button'
 
     @staticmethod
     def get_string(value: int, type: str) -> str:
-        type_mapping: dict = {
+        type_mapping = {
             'heart.rate': f"{value}ppm",
             'distance': f"{value}m" if value >= 100 else f"{value}km",
             'calories': f"{value}cals",
@@ -30,11 +28,12 @@ class Duration:
 
     @staticmethod
     def get_value(duration: str) -> Union[int, None]:
-        for key in TYPE_MAPPING.keys():
+        for key, _ in TYPE_MAPPING.items():
             if key in duration:
-                return int(float(duration.split(key)[0]) if key != 'reps' else duration.split(key)[0])
-        if ':' in duration:
+                return int(float(duration.split(key)[0]))
+        if Duration.is_time(duration):
             return Time(duration).to_seconds()
+        return None
 
     @staticmethod
     def is_time(string: str) -> bool:
