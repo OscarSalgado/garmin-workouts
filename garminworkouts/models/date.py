3b3c07185d06a578ed2356140e3dc6ca1ffee864
Oscar Salgado <osalgado@ikerlan.es>
2025-01-13 21:04:19 +0100

    Refactor date parsing logic in get_date function and enhance test coverage with parameterized tests

--- garminworkouts/models/date.py
+++ garminworkouts/models/date.py
@@ -3,11 +3,11 @@ from datetime import date, timedelta
 
 def get_date(name, race, date_ini) -> tuple[date, int, int]:
     try:
-        parts = name.lstrip('RD').split('_')
-        week = int(parts[0])
+        parts = name.replace('-', '_').lstrip('RDW').split('_')
+        week = int(parts[0].lstrip('D')) if len(parts) > 1 else 0
         week = -week if ('R' in name or 'D' in name) else week
-        day = int(parts[1]) if len(parts) > 1 else int(name.split('D')[1].split('-')[0])
-        day = day if day < 8 else 0
+        day = int(parts[1].lstrip('D')) if len(parts) > 1 else int(parts[0].lstrip('D'))
+        day = day if day < 8 else day if len(parts) == 1 else 0
         return race - timedelta(weeks=week + 1) + timedelta(days=day), week, day
     except ValueError:
         if isinstance(date_ini, dict):
