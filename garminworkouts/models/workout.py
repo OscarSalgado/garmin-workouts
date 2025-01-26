1fae3d066b8d58e59a5db6512cb681994947ff6c
Oscar Salgado <osalgado@ikerlan.es>
2025-01-26 21:48:03 +0100

    Refactor standard_density method to use a static method for string replacement in file

--- garminworkouts/models/workout.py
+++ garminworkouts/models/workout.py
@@ -303,22 +303,13 @@ class Workout(object):
             return 0.0
 
     def standard_density(self, interval, recovery, rest, warmup, cooldown, other, maxIF):
-        def replace_string_in_file(file_path: str, search_string: str, replace_string: str) -> None:
-            with open(file_path, 'r') as file:
-                file_data = file.read()
-
-            new_data = file_data.replace(search_string, replace_string)
-
-            with open(file_path, 'w') as file:
-                file.write(new_data)
-
         if (recovery + rest) == 0 or interval == 0:
             return 0.0
         D = interval / (recovery + rest)
         if maxIF <= 5.0:  # R3
             Dmax = 5.71
             if D > Dmax:
-                replace_string_in_file(
+                self.replace_string_in_file(
                     self.config.get("file"),
                     str(timedelta(seconds=round(interval/D))),
                     str(timedelta(seconds=(round(interval/Dmax) // 15) * 15)))
@@ -330,7 +321,7 @@ class Workout(object):
         elif maxIF <= 9.0:  # R3+
             Dmax = 2.48
             if D > Dmax:
-                replace_string_in_file(
+                self.replace_string_in_file(
                     self.config.get("file"),
                     str(timedelta(seconds=round(interval/D))),
                     str(timedelta(seconds=(round(interval/Dmax) // 15) * 15)))
@@ -342,7 +333,7 @@ class Workout(object):
         elif maxIF <= 15.0:  # R4
             Dmax = 1.38
             if D > Dmax:
-                replace_string_in_file(
+                self.replace_string_in_file(
                     self.config.get("file"),
                     str(timedelta(seconds=round(interval/D))),
                     str(timedelta(seconds=(round(interval/Dmax) // 15) * 15)))
@@ -354,7 +345,7 @@ class Workout(object):
         else:  # R5/R6
             Dmax = 0.3
             if D > Dmax:
-                replace_string_in_file(
+                self.replace_string_in_file(
                     self.config.get("file"),
                     str(timedelta(seconds=round(interval/D))),
                     str(timedelta(seconds=(round(interval/Dmax) // 15) * 15)))
@@ -363,6 +354,18 @@ class Workout(object):
                     f'Minimum recovery time: {str(timedelta(seconds=round(interval/Dmax)))}'
                 )
             return 89.204 * D - 27.562
+
+    @staticmethod
+    def replace_string_in_file(file_path: str, search_string: str, replace_string: str) -> None:
+        if file_path is None:
+            return None
+        with open(file_path, 'r') as file:
+            file_data = file.read()
+
+        new_data = file_data.replace(search_string, replace_string)
+
+        with open(file_path, 'w') as file:
+            file.write(new_data)
 
     def calculate_ECOs(self, ECOs, intensity_factor_list):
         if len(intensity_factor_list) > 1 and intensity_factor_list[-2] > 0:
