63cb63c3a4daa41b39a17508041d18736d76fd3c
Oscar Salgado <osalgado@ikerlan.es>
2025-06-11 19:54:05 +0200

    Implement target setting and metrics calculation for workouts; enhance event updates to include weekly targets based on mileage and duration.

--- intervalsicu/intervals/client.py
+++ intervalsicu/intervals/client.py
@@ -18,15 +18,73 @@ class IntervalsClient(IntervalsWorkout):
             trainings, plan_id=plan_folder.get('id', None), day_a=start_date, day_b=end_date)
         self.upload_workouts(formatted_payload)
 
+    def set_targets(self, workouts, day_a: date, day_b: date) -> None:
+        # Encuentra los días que son lunes entre day_a y day_b
+        data = []
+        for sport in ['Run', 'Swim', 'Ride']:
+            mondays, mileage, duration = self.get_sport_target_metrics(workouts, day_a, day_b, sport)
+
+            self.set_targets_(data, sport, mondays, mileage, duration)
+        self.upload_events(data)
+
+    def get_sport_target_metrics(self, workouts, day_a, day_b, sport):
+        mondays = []
+        current_day = day_a
+        while current_day <= day_b:
+            if current_day.weekday() == 0:  # 0 es lunes
+                mondays.append(current_day)
+            current_day += timedelta(days=1)
+        sundays = [monday + timedelta(days=6) for monday in mondays]  # Añade los domingos correspondientes
+        # Inicializa un vector de ceros del tamaño de mondays
+        mileage = [0] * len(mondays)
+        duration = [0] * len(mondays)
+
+        for w in workouts:
+            if self.format_sport(w) == sport:
+                d, _, _ = w.get_workout_date()
+                for monday in mondays:
+                    index = mondays.index(monday)
+                    sunday = sundays[index]
+                    if monday <= d <= sunday:
+                        # Suma el kilometraje y la duración al índice correspondiente
+                        if w.mileage is not None:
+                            mileage[index] += w.mileage if sport == 'Run' else 0
+                        if w.duration is not None:
+                            duration[index] += w.duration.seconds
+                        break
+        return mondays, mileage, duration
+
+    def set_targets_(self, data, sport, mondays, mileage, duration):
+        for monday in mondays:
+            if monday >= date.today():
+                index = mondays.index(monday)
+                data.append({
+                        "category": "TARGET",
+                        "type": sport,
+                        "name": "Weekly",
+                        "start_date_local": f"{monday.isoformat()}T00:00:00",
+                        "load_target": 0,
+                        "time_target": duration[index],
+                        "distance_target": mileage[index] * 1000,
+                    })
+
     def update_events_(self, workouts: list[Workout], plan_folder) -> None:
         trainings: dict = {'trainings': []}
         trainings['trainings'] = workouts
 
         start_date = date.today() + timedelta(days=1)
+
+        # Calculate the Monday before or equal to start_date
+        if start_date.weekday() == 0:
+            monday_before = start_date
+        else:
+            monday_before = start_date - timedelta(days=start_date.weekday())
+
         # Find the next Sunday after start_date
         days_until_sunday = (6 - start_date.weekday()) % 7
         end_date = start_date + timedelta(days=days_until_sunday) + timedelta(weeks=3)
         self.delete_range_events()
+        self.set_targets(workouts, day_a=monday_before, day_b=end_date)
         formatted_payload = self.format_training_data(
             trainings, plan_id=plan_folder.get('id', None), day_a=start_date, day_b=end_date)
         self.upload_events(formatted_payload)
