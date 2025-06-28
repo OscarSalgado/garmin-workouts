f5cec8921485be331b9ef5fe370a9db164d1b981
Oscar Salgado <osalgado@ikerlan.es>
2025-06-28 08:35:50 +0200

    Refactor update_workouts and sync_plan_folder methods for improved plan handling and workout synchronization

--- intervalsicu/intervals/client.py
+++ intervalsicu/intervals/client.py
@@ -7,16 +7,38 @@ from intervalsicu.intervals.workout import IntervalsWorkout
 
 class IntervalsClient(IntervalsWorkout):
 
-    def update_workouts_(self, workouts: list[Workout], plan_folder) -> None:
-        trainings: dict = {'trainings': []}
-        trainings['trainings'] = workouts
+    def update_workouts(self, workouts: list[Workout], plan: str) -> None:
+        existing_plans = self.list_plans()
 
-        start_date_str = plan_folder.get('start_date_local', None)
-        start_date = date.fromisoformat(start_date_str.split('T')[0]) if start_date_str else date.today()
-        end_date = start_date + timedelta(weeks=18)
-        formatted_payload = self.format_training_data(
-            trainings, plan_id=plan_folder.get('id', None), day_a=start_date, day_b=end_date)
-        self.upload_workouts(formatted_payload)
+        plan_folder = self.get_plan_folder(workouts, plan, existing_plans)
+        self.update_events_(workouts, plan_folder)
+
+    def get_plan_folder(self, workouts, plan, existing_plans):
+        d = date(year=3000, month=1, day=1)
+        for workout in workouts:
+            day_d, *_ = workout.get_workout_date()
+            if day_d < d:
+                d = day_d
+
+        if plan in existing_plans.keys():
+            plan_folder = existing_plans[plan]
+            if len(plan_folder.get('children', [])) == 0:
+                self.delete_plan(plan_folder.get('id', None))
+                wellness = self.get_wellness(date=d)
+                plan_folder = self.create_plan(plan_name=plan,
+                                               d=d,
+                                               ctl=wellness.get('ctl', -1),
+                                               atl=wellness.get('atl', -1))
+                logging.info("Plan '%s'", plan)
+        else:
+            wellness = self.get_wellness(date=d)
+            plan_folder = self.create_plan(plan_name=plan,
+                                           d=d,
+                                           ctl=wellness.get('ctl', -1),
+                                           atl=wellness.get('atl', -1))
+            logging.info("Plan '%s'", plan)
+
+        return plan_folder
 
     def update_events_(self, workouts: list[Workout], plan_folder) -> None:
         trainings: dict = {'trainings': []}
@@ -26,7 +48,6 @@ class IntervalsClient(IntervalsWorkout):
 
         # Calculate the Monday before or equal to start_date
         monday_before = start_date - timedelta(days=start_date.weekday())
-        start_date = monday_before if monday_before <= start_date else start_date
 
         # Find the next Sunday after start_date
         days_until_sunday = (6 - start_date.weekday()) % 7
@@ -34,49 +55,41 @@ class IntervalsClient(IntervalsWorkout):
         self.delete_range_events(monday_before, end_date)
         self.set_targets(workouts, day_a=monday_before, day_b=end_date)
         formatted_payload = self.format_training_data(
-            trainings, plan_id=plan_folder.get('id', None), day_a=start_date, day_b=end_date)
+            trainings, plan_folder=plan_folder, day_a=start_date, day_b=end_date)
         if len(formatted_payload) > 0:
             self.upload_events(formatted_payload)
         else:
             logging.warning("No events to upload for plan '%s'", plan_folder.get('name', 'Unknown Plan'))
 
-    def update_workouts(self, workouts: list[Workout], plan: str) -> None:
-        existing_plans = self.list_plans()
-
-        plan_folder = self.create_plan_folder(workouts, plan, existing_plans)
-        self.update_events_(workouts, plan_folder)
-
     def update_training_plan(self, workouts: list[Workout], plan: str) -> None:
         existing_plans = self.list_plans()
 
-        plan_folder = self.create_plan_folder(workouts, plan, existing_plans)
-        workouts_by_name: dict[str, Workout] = {w.get_workout_name(): w for w in workouts}
-        self.sync_plan_folder(workouts, workouts_by_name, plan_folder)
+        plan_folder = self.get_plan_folder(workouts, plan, existing_plans)
+        self.sync_plan_folder(workouts, plan_folder)
 
-    def sync_plan_folder(self, workouts, workouts_by_name, plan_folder):
-        self.update_workouts_(workouts=workouts, plan_folder=plan_folder)
-        existing_workouts_by_name: dict = {w.get('name'): w for w in self.list_workouts()}
-        for workout_name in existing_workouts_by_name.keys():
-            id = existing_workouts_by_name[workout_name].get('id', None)
-            w = workouts_by_name.get(workout_name)
-            if w:
-                d, _,  _ = w.get_workout_date()
-                days_diff = (d - date.fromisoformat(plan_folder['start_date_local'].split('T')[0])).days
-                self.update_workout(id, days_diff)
+    def sync_plan_folder(self, workouts, plan_folder):
+        workout_list = plan_folder.get('children', [])
+        existing_workouts_by_name: dict = {w.get('name', ''): w for w in workout_list} if workout_list else {}
+        curated_list = []
 
-    def create_plan_folder(self, workouts, plan, existing_plans):
-        d = date(year=3000, month=1, day=1)
         for workout in workouts:
-            day_d, *_ = workout.get_workout_date()
-            if day_d < d:
-                d = day_d
+            workout_name = workout.get_workout_name()
+            if workout_name in existing_workouts_by_name:
+                existing_workouts_by_name.pop(workout_name, None)
+            else:
+                curated_list.append(workout)
 
-        if plan in existing_plans.keys():
-            plan_folder = existing_plans[plan]
-            self.delete_plan(plan_folder.get('id', None))
+        if len(curated_list) > 0:
+            trainings: dict = {'trainings': []}
+            trainings['trainings'] = curated_list
 
-        wellness = self.get_wellness(date=d)
-        plan_folder = self.create_plan(plan_name=plan, d=d, ctl=wellness.get('ctl', -1), atl=wellness.get('atl', -1))
-        logging.info("Plan '%s'", plan)
+            start_date_str = plan_folder.get('start_date_local', None)
+            start_date = date.fromisoformat(start_date_str.split('T')[0]) if start_date_str else date.today()
+            end_date = start_date + timedelta(weeks=18)
+            formatted_payload = self.format_training_data(
+                trainings, plan_folder=plan_folder, day_a=start_date, day_b=end_date)
+            self.upload_workouts(formatted_payload)
 
-        return plan_folder
+        for workout in existing_workouts_by_name:
+            id = workout.get('id', None)
+            self.delete_workout(id)
